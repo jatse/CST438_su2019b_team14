2836b5b154dbac6baeb36ccb87df2f75ea2ec303
jatse <jatse@csumb.edu>
2019-08-04 20:20:35 +0000

    use HTTParty executable instead of class

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,18 +1,4 @@
-require 'httparty'
-
 class OrdersController < ApplicationController
-    
-    #HTTParty class for common request configuations
-    class Connection
-        include HTTParty
-        
-        headers 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json'
-        format :json
-        #base_uri not set.
-        #Use 'http://localhost:8081/' for customer API base_uri
-        #Use 'http://localhost:8082/' for item API base_uri
-    end
-    
     #===========================================================================
     #Adds a new order to database.
     #POST /orders
